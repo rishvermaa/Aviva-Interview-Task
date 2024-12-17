d0a81b93397f5ffe54c49f69e683d9eefe9b332c
Rishabh Verma <96272516+rishvermaa@users.noreply.github.com>
2024-12-17 19:32:50 +0000

    Word_Count.py

--- code/Word_Count.py
+++ code/Word_Count.py
@@ -20,8 +20,8 @@ def count_common_words(json_file: str):
     def find_words(text):
         return re.findall(r'\b\w{5,}\b', text)
 
-    #Counting all words with 5+ letters across all petitions
-    all_words = []  #List to hold all words from all petitions
+    #Identifying all words with 5+ letters across all petitions
+    all_words = [] 
     for petition in petitions:
         abstract = petition.get('abstract', {}).get('_value', '')
         label = petition.get('label', {}).get('_value', '')
@@ -29,7 +29,7 @@ def count_common_words(json_file: str):
         #Extracting words and adding them to the list
         all_words.extend(find_words(combined))
 
-    #Finding the 20 most common words
+    #Counting and finding the most common 20 words
     repitive_words = [word for word, _ in Counter(all_words).most_common(20)]
 
     #Counting words for each petition and assigning unique ID
